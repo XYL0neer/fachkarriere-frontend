4d5e8d2f89d1ccb0051c208528bdc46c8e590b84
Eugen Soliar <eugen.soliar@exxeta.com>
2023-05-24 16:37:28 +0200

    added a feature to add new skill

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,18 +1,96 @@
+import { useState } from "react";
+interface Skill {
+  id: number;
+  name: string | undefined;
+}
+const initialList: Skill[] = [
+  {
+    id: 1,
+    name: "Angular",
+  },
+  {
+    id: 2,
+    name: "React",
+  },
+  {
+    id: 3,
+    name: "Scrum",
+  },
+  {
+    id: 4,
+    name: "java",
+  },
+  {
+    id: 5,
+    name: "Full-Stack",
+  },
+  {
+    id: 6,
+    name: "Presentation",
+  },
+  {
+    id: 7,
+    name: "Coach",
+  },
+];
+
 export default function Skills() {
+  const [list, setList] = useState(initialList);
+  const [text, setText] = useState();
+
+  const getRandomInt = () => {
+    return Math.floor(Math.random() * 100);
+  };
+
+  // eslint-disable-next-line
+  const handleAddSkill = (e: any) => {
+    // eslint-disable-next-line
+    e.preventDefault();
+    const newList = list.concat({ id: getRandomInt(), name: text });
+    // eslint-disable-next-line
+    setList(newList);
+    console.log("clicked");
+  };
+
+  // eslint-disable-next-line
+  const handleChange = (e: any) => {
+    // eslint-disable-next-line
+    console.log("event", e.target.value);
+    // eslint-disable-next-line
+    setText(e.target.value);
+  };
+
   return (
     <>
-      <div className="my-2 flex items-center gap-2">
-        <span>Skills</span>
-        <div className="h-[2px] w-full bg-white"></div>
+      <div className="my-4">
+        <h2 className="mx-1 text-2xl">Skills</h2>
+        <form className="flex gap-4" onSubmit={handleAddSkill}>
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="Type here"
+            className="input-bordered input w-full max-w-xs"
+          />
+          <button
+            type="submit"
+            className="btn-circle btn justify-center bg-white align-middle"
+          >
+            <span>+</span>
+          </button>
+        </form>
       </div>
-      <div className="flex flex-wrap justify-between">
-        <span className="badge rounded-3xl">React</span>
-        <span className="badge rounded-3xl">Angular</span>
-        <span className="badge rounded-3xl">Scrum</span>
-        <span className="badge rounded-3xl">java</span>
-        <span className="badge rounded-3xl">Full-Stack</span>
-        <span className="badge rounded-3xl">Presentation</span>
-        <span className="badge rounded-3xl">Coach</span>
+
+      <div className="h-[2px] w-full bg-white"></div>
+
+      <div className="flex flex-wrap ">
+        {list.map((item) => (
+          <span
+            key={item.id}
+            className="badge m-2 h-8 rounded-3xl bg-white text-black"
+          >
+            <span className="mx-1">{item.name}</span>
+          </span>
+        ))}
       </div>
     </>
   );
